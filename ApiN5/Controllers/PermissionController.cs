fde8f6037a547ba3407df350503c519a6cdd683d
Donaghy Amachi <donaghyalexis.amachi@unir.net>
2024-01-17 16:37:17 -0500

    Api Project

---
+++ ApiN5/Controllers/PermissionController.cs
@@ -0,0 +1,82 @@
+﻿using Core.RequestParams;
+using Core.Services;
+using Core.Services.Impl;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiN5.Controllers
+{
+    [Route("permission")]
+    public class PermissionController : Controller
+    {
+        private readonly IPermissionService _permissionService;
+        private readonly ILogger _logger;
+        public PermissionController(IPermissionService permissionService,ILogger<PermissionController> logger)
+        {
+            _permissionService = permissionService;
+            _logger = logger;
+        }
+        [Route("request")]
+        [HttpPost]
+        public async Task<ActionResult> RequestPermission(PermissionRequestParams param)
+        {
+            try
+            {
+                var response = await _permissionService.CreatePermission(param);
+                return Ok(response);
+            }
+            catch (BadHttpRequestException ex)
+            {
+                Console.WriteLine(ex);
+                return new BadRequestObjectResult(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return StatusCode(500, $"Internal error:{ex.Message}");
+            }
+        }
+
+        [Route("modify")]
+        [HttpPut]
+        public async Task<ActionResult> ModifyPermission(PermissionRequestParams param)
+        {
+            try
+            {
+                var response = await _permissionService.CreatePermission(param);
+                return Ok(response);
+            }
+            catch (BadHttpRequestException ex)
+            {
+                Console.WriteLine(ex);
+                return new BadRequestObjectResult(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return StatusCode(500, $"Internal error:{ex.Message}");
+            }
+        }
+
+        [Route("get")]
+        [HttpGet]
+        public async Task<ActionResult> GetPermissions(int? permissionId)
+        {
+            _logger.LogInformation("ORALE MANO");
+            try
+            {
+                var response = await _permissionService.GetPermission(permissionId);
+                return Ok(response);
+            }
+            catch (BadHttpRequestException ex)
+            {
+                Console.WriteLine(ex);
+                return new BadRequestObjectResult(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return StatusCode(500, $"Internal error:{ex.Message}");
+            }
+        }
+    }
+}
---
+++ ApiN5/Controllers/PermissionTypeController.cs
@@ -0,0 +1,80 @@
+﻿using Core.RequestParams;
+using Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiN5.Controllers
+{
+    [Route("permissiontype")]
+    public class PermissionTypeController : Controller
+    {
+        private readonly IPermissionTypeService _permissionTypeService;
+        
+        public PermissionTypeController(IPermissionTypeService permissionTypeService)
+        {
+            _permissionTypeService = permissionTypeService;
+        }
+
+        [Route("request")]
+        [HttpPost]
+        public async Task<ActionResult> RequestPermission(PermissionTypeRequestParams param)
+        {
+            try
+            {
+                var response = await _permissionTypeService.CreatePermissionType(param);
+                return Ok(response);
+            }
+            catch (BadHttpRequestException ex)
+            {
+                Console.WriteLine(ex);
+                return new BadRequestObjectResult(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return StatusCode(500, $"Internal error:{ex.Message}");
+            }
+        }
+        
+        [Route("modify")]
+        [HttpPut]
+        public async Task<ActionResult> ModifyPermissionType(PermissionTypeRequestParams param)
+        {
+            try
+            {
+                var response = await _permissionTypeService.CreatePermissionType(param);
+                return Ok(response);
+            }
+            catch (BadHttpRequestException ex)
+            {
+                Console.WriteLine(ex);
+                return new BadRequestObjectResult(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return StatusCode(500, $"Internal error:{ex.Message}");
+            }
+        }
+        
+        [Route("get")]
+        [HttpGet]
+        public async Task<ActionResult> GetPermissionTypes(int? permissionTypeId)
+        {
+            try
+            {
+                var response = await _permissionTypeService.GetPermissionTypes(permissionTypeId);
+                return Ok(response);
+            }
+            catch (BadHttpRequestException ex)
+            {
+                Console.WriteLine(ex);
+                return new BadRequestObjectResult(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return StatusCode(500, $"Internal error:{ex.Message}");
+            }
+        }
+    }
+}
