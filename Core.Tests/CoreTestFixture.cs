094da04404d78ec7e4f8999107921a9655ac52cf
Donaghy Amachi <donaghyalexis.amachi@unir.net>
2024-01-17 19:57:33 -0500

    Adding UnitTest

---
+++ Core.Tests/CoreTestFixture.cs
@@ -0,0 +1,46 @@
+﻿using Core.DbModel.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.Configuration;
+
+namespace Core.Tests
+{
+    public class CoreTestFixture
+    {
+        private readonly Random _random;
+        private readonly DataContext _context;
+        private readonly IConfiguration _configuration;
+
+        public CoreTestFixture()
+        {
+            _random=new Random(Environment.TickCount);
+
+            var options = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .Build();
+
+            _context = new DataContext(options, configuration);
+            _configuration = configuration;
+        }
+        public DataContext CreateNewDbContext()
+        {
+            var options = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .ConfigureWarnings(x => x.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+                .Options;
+
+            //var inMemorySettings = new Dictionary<string, string> {
+            //    {"ENTORNO", "test"},
+            //};
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                //.AddInMemoryCollection(inMemorySettings)
+                .Build();
+
+            return new DataContext(options, configuration);
+        }
+    }
+}
---
+++ Core.Tests/Services/PermissionServiceTest.cs
@@ -0,0 +1,38 @@
+﻿using Core.RequestParams;
+using Core.Services.Impl;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Core.Tests.Services
+{
+    public class PermissionServiceTest : IClassFixture<CoreTestFixture>
+    {
+        private readonly CoreTestFixture _fixture;
+        private readonly ILogger<PermissionService> _logger;
+
+        public PermissionServiceTest(CoreTestFixture fixture)
+        {
+            _fixture = fixture;
+            _logger = new Mock<ILogger<PermissionService>>().Object;
+        }
+
+        [Fact(DisplayName = "Should create permission")]
+        public async void CreatePermission()
+        {
+            var context = _fixture.CreateNewDbContext();
+
+            var sut = new PermissionService(context, _logger);
+            
+            var requestPermissionParams = new PermissionRequestParams
+            {
+                EmployeeForename = "Dónaghy",
+                EmployeeSurname = "Amachi",
+                PermissionTypeId = 1,
+                PermissionDate = default
+            };
+            var res = await sut.CreatePermission(requestPermissionParams);
+
+            Assert.NotEqual(0, res.Id);
+        }
+    }
+}
